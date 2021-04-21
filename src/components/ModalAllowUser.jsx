9d92923c9dea3c4a5e1bfb0b21610a6d44f43ca3
Ermal-code <72197798+Ermal-code@users.noreply.github.com>
2021-04-21 22:03:59 +0200

    Update ModalAllowUser.jsx

--- src/components/ModalAllowUser.jsx
+++ src/components/ModalAllowUser.jsx
@@ -7,16 +7,18 @@ const ModalAllowUser = ({ show, handleClose }) => {
       <Modal.Header closeButton></Modal.Header>
       <Modal.Body>
         <p>
-          Do you want to allow this doctor access to your profile and documents
+          Do you want to allow this doctor access to your profile and documents!
         </p>
       </Modal.Body>
-      <Modal.Footer>
-        <Button variant="outline-primary" onClick={handleClose}>
-          Yes
-        </Button>
-        <Button variant="outline-primary" onClick={handleClose}>
-          No
-        </Button>
+      <Modal.Footer className="d-flex justify-content-center">
+        <div className="d-flex justify-content-between w-50">
+          <Button variant="success" onClick={handleClose}>
+            Yes
+          </Button>
+          <Button variant="danger" onClick={handleClose}>
+            No
+          </Button>
+        </div>
       </Modal.Footer>
     </Modal>
   );
